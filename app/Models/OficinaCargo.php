2395848773da2969e1fe2a71f2a07c153efe1dae
CarlosFancoHG <131261473+Franko19Rg4@users.noreply.github.com>
2024-08-21 12:22:20 -0500

    feat: Agregando las relaciones al Modelo OficinaCargo

--- app/Models/OficinaCargo.php
+++ app/Models/OficinaCargo.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class OficinaCargo extends Model
 {
@@ -18,4 +19,11 @@ class OficinaCargo extends Model
     ];
 
     public $timestamps = false;
+    
+    // Relaciones
+
+    public function cargo(): BelongsTo
+    {
+        return $this->belongsTo(Cargo::class, 'id_ofi');
+    }
 }
