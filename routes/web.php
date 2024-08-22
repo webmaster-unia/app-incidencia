9c5a7b5f7eeb970efaef7b1b0de23ec514d7339d
Jamt Mendoza <43344540+iamjamtt@users.noreply.github.com>
2024-08-21 23:12:39 -0500

    feat: Agregar ruta que redirige al home

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,12 @@
 use Illuminate\Support\Facades\Route;
 use Livewire\Volt\Volt;
 
+Route::redirect('/', '/home');
+
 Volt::route('/login', 'auth.login')
     ->name('login');
 
-Volt::route('/', 'home.index')
+Volt::route('/home', 'home.index')
     ->name('home.index');
 
 //
