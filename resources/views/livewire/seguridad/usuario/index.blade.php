dca1058432501dfe62dc0189e73e64e1ed0b0b01
CarlosFancoHG <alexisgrandez69@gmail.com>
2024-09-05 10:26:07 -0500

    add: creacion de los componentes seguridad/usuario, seguridad/rol

---
+++ resources/views/livewire/seguridad/usuario/index.blade.php
@@ -0,0 +1,17 @@
+<?php
+
+use Livewire\Volt\Component;
+use Livewire\Attributes\{Layout, Title, Url, Validate};
+use App\Models\{Usuario,Rol,Trabajador};
+use Livewire\WithPagination;
+
+new 
+#[Layout('components.layouts.app')]
+#[Title('Roles | SIGEIN OTI')]
+class extends Component {
+    //
+}; ?>
+
+<div>
+    //
+</div>
