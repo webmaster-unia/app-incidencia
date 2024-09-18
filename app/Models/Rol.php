f28b369d2af943771dd4bfc61449c43b9e09fb83
CarlosFancoHG <alexisgrandez69@gmail.com>
2024-09-18 09:14:21 -0500

    upd: creación de las relaciones

--- app/Models/Rol.php
+++ app/Models/Rol.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Rol extends Model
 {
@@ -24,4 +26,23 @@ class Rol extends Model
     protected $casts = [
         'activo_rol' => 'boolean'
     ];
+    
+    // Relaciones
+    public function usuarios(): HasMany
+    {
+        return $this->hasMany(Usuario::class, 'id_usu');
+    }
+    
+    public function acciones(): BelongsToMany
+    {
+        return $this->belongsToMany(Accion::class, 'tbl_rol_accion', 'id_rol', 'id_accion');
+    }
+    
+    // alcance
+    public function scopeSearch($query, $search)
+    {
+        if ($search) {
+            return $query->where('nombre_rol', 'LIKE', "%$search%");
+        }
+    }
 }
