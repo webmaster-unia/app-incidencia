c092d91f1c30642ffd901c2670c09c13afec3498
CarlosFancoHG <alexisgrandez69@gmail.com>
2024-09-24 10:22:43 -0500

    upd: solución al componente Rol

--- app/Models/Rol.php
+++ app/Models/Rol.php
@@ -6,8 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Database\Eloquent\Relations\BelongsToMany;
-use Illuminate\Database\Eloquent\Relations\HasMany;
+
 
 class Rol extends Model
 {
